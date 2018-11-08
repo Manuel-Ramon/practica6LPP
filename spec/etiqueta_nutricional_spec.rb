405b978e1f706fb83434e7ddcfb110178fae4389
Ramon Regalado <alu0100283433@ull.edu.es>
2018-11-08 16:41:52 +0000

    Intentando instanciar una clase Etiqueta con rspec que todavía no existe en el fuente

--- spec/etiqueta_nutricional_spec.rb
+++ spec/etiqueta_nutricional_spec.rb
@@ -1,1 +1,11 @@
-require 'lib/etiqueta_nutricional.rb'
+#NOTA: la tarea en el Rakefile se llama 'TDD' para inicializarla escribir 'rake TDD' en la terminal (situados previamente donde esté dicho fichero)
+
+#1
+require 'lib/etiqueta_nutricional'
+
+#2
+
+#Llamada al constructor (instanciación de clase)
+context  Etiqueta do
+	label = Etiqueta.new("Galletas",23,11,66,37,10,1)
+end
